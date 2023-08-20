6bacd32b8f6330aa31b31652bdef3b3b4861849f
Dylan Fine <dylanfine@Dylans-MacBook-Air-2.local>
2023-08-20 14:43:39 -0700

    andrew ses email identity

--- ses.tf
+++ ses.tf
@@ -2,3 +2,8 @@ resource "aws_ses_email_identity" "dylan_email" {
   email = "dylanjfine@gmail.com"
 
 }
+
+resource "aws_ses_email_identity" "andrew_email" {
+  email = "andrewboppart@gmail.com"
+
+}
